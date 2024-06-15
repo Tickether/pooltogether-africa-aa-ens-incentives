cb72f342d387df3a726729a0b5324d084b957561
Tickether <tickether@gmail.com>
2024-06-15 16:24:58 +0000

    feat: send email x db + w/ hash sync

--- src/utils/biconomy/smartUserOP.ts
+++ src/utils/biconomy/smartUserOP.ts
@@ -1,14 +1,16 @@
 import { walletClient } from '../viem/client.js'
 import { BiconomySmartAccountV2, PaymasterMode, Transaction, UserOpResponse, createSmartAccountClient } from '@biconomy/account'
-import dotenv from 'dotenv'
-import { QUOTER, Recipient, SWAP_ROUTER, USDC, WETH, przUSDC } from '../constants/addresses.js';
+import { Recipient, przUSDC } from '../constants/addresses.js';
 import { claimYieldFeeShares } from '../incentives/claimYieldFeeShares.js';
 import { transfer } from '../incentives/transfer.js';
 import { yieldFeeBalance } from '../incentives/yieldFeeBalance.js';
 import { getPoolers } from '../pooler/getPoolers.js';
 import { getPooler } from '../pooler/getPooler.js';
-import { vaultTWAB } from '../incentives/vaultTWAB.js';
 import { poolerTWAB } from '../incentives/poolerTWAB.js';
+import { postPoolerIncentives } from '../pooler/postPoolerIncentives.js';
+import { formatUnits } from 'viem';
+import { sendEmail } from '../../mail/sendEmail.js';
+import dotenv from 'dotenv'
 
 dotenv.config();
 
@@ -39,12 +41,14 @@ export const createSmartAccount = async () => {
 export interface TWAB {
     address: `0x${string}`
     email: string
-    shares: bigint
+    ens: string
+    sharesTWAB: bigint
+    sharesAmount: bigint
     transferTx: Transaction
 }
 function chunkUserOpsArray(arr: Transaction[], maxTxData: number) {
     const chunkUserOps = [];
-    for (let i = 0; i < arr.length; i += maxTxData) {
+    for (let i = 1; i < arr.length; i += maxTxData) {
         chunkUserOps.push(arr.slice(i, i + maxTxData));
     }
     return chunkUserOps;
@@ -81,7 +85,8 @@ const poolIncentivesDistro = async() => {
     let poolersTWAB: TWAB[]
     // get all users (get twab and filter)
     const poolers = await getPoolers()
-    let vaultTWAB_ //= await vaultTWAB(przUSDC, BigInt('1000000'), BigInt('1000000'))
+    let vaultTWAB_: bigint
+    let txnHashes: string[]
 
     for (let i = 0; i < poolers!.length; i++) {
         //calc amount to send and push
@@ -94,7 +99,9 @@ const poolIncentivesDistro = async() => {
             const TWAB = {
                 address: poolerAddress,
                 email: pooler?.email!,
-                shares: poolerTWAB_,
+                ens: pooler?.ens!,
+                sharesTWAB: poolerTWAB_,
+                sharesAmount: BigInt(0),
                 transferTx: {to: '', data: ''}
             }
             //push no zero tabs and shares into anew array
@@ -103,18 +110,16 @@ const poolIncentivesDistro = async() => {
     }
 
     vaultTWAB_ = poolersTWAB!.reduce((totalTWAB: bigint, twab: TWAB ) => {
-        return totalTWAB + twab.shares;
+        return totalTWAB + twab.sharesTWAB;
     }, BigInt(0))
 
     for (let i = 0; i < poolersTWAB!.length; i++) {
         const poolerTWAB_ = poolersTWAB![i];
         //calculate amonunt amount to send into interface
-        const userAmount = (poolerTWAB_.shares * amount) / vaultTWAB_
+        const userAmount = (poolerTWAB_.sharesTWAB * amount) / vaultTWAB_
+        poolerTWAB_.sharesAmount = userAmount
         const transferTx = transfer(poolerTWAB_.address, userAmount)
-        poolerTWAB_.transferTx = transferTx
-        //post db
-        //send emails
-        
+        poolerTWAB_.transferTx = transferTx        
     }
     // generate arrays of transfer tx calldata to send shares out max 450
     
@@ -137,10 +142,23 @@ const poolIncentivesDistro = async() => {
     // later we can resolve the promises to get user operation receipts
     const userOpResponses: UserOpResponse[] = await Promise.all(txPromises);
     userOpResponses.forEach(async (userOpResponse) => {
+        const { transactionHash } = await userOpResponse.waitForTxHash();
+        txnHashes.push(transactionHash!)
         const { wait } = userOpResponse;
         const receipt = await wait();
         console.log("Receipt: ", receipt);
     })
+
+    for (let i = 0; i < poolersTWAB!.length; i++) {
+        const twab = poolersTWAB![i];
+        const chunkIndex = (Math.floor(i / 400))
+        const transactionHash = txnHashes![chunkIndex];
+        const amount = formatUnits(twab.sharesAmount, 6)
+        //post db
+        await postPoolerIncentives(twab.address, Recipient, transactionHash, Number(amount).toFixed(2), 'incentive')
+        //send emails
+        await sendEmail(twab.email, twab.ens, Number(amount).toFixed(2))
+    }
 }
 
 
